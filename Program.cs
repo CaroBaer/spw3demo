0b3c54e97a406acfb3377c2715bb2ad473dae156
Caroline Bär <caroline.baer@students.bs-linz2.ac.at>
2023-11-30 14:00:21 +0100

    test branche feature

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git is cool.?? Are you sure??");
+Console.WriteLine("Hello, World! Git is cool.?? Are you sure?? Neu Feature");
