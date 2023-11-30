8fe25c41211fe755eb1503762cb79b02454ce843
Caroline Bär <caroline.baer@students.bs-linz2.ac.at>
2023-11-30 16:30:01 +0100

    update second change and third

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git is cool.?? Are you sure?? m1 f2 change remote");
+
+Console.WriteLine("Hello, World! Git is cool.?? Are you sure?? m1 f2 change remote fix");
+
 
